8e2cf10b87935548588048743d8e45460e7eb38f
manish-bits-70032 <2021mt70032@wilp.bits-pilani.ac.in>
2023-03-06 03:00:08 +0530

    adding raw code to new branch

---
+++ test2/src/test2/rawcode.java
@@ -0,0 +1,14 @@
+package test2;
+
+public class rawcode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num1=10;
+		int num2=20;
+		
+		System.out.println(num1+num2);
+	}
+
+}
